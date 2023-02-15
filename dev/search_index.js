ad2670717ba8d150316a360fbcaaf319e2d91b72
Documenter.jl <documenter@juliadocs.github.io>
2023-02-15 20:15:19 +0000

    build based on c112b6d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProjectSupervisor","category":"page"},{"location":"#ProjectSupervisor","page":"Home","title":"ProjectSupervisor","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProjectSupervisor.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProjectSupervisor]","category":"page"}]
+}
